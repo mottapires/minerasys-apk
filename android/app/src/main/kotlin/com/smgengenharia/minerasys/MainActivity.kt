88f0e1ca56cb1b9b110977225e69bfc402e8a964
mottapires <70417268+mottapires@users.noreply.github.com>
2025-11-08 17:34:17 -0300

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/smgengenharia/minerasys/MainActivity.kt
+++ android/app/src/main/kotlin/com/smgengenharia/minerasys/MainActivity.kt
@@ -4,3 +4,12 @@ import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
 }
+```
+
+---
+
+## Se o arquivo não existir ou estiver vazio:
+
+Crie o arquivo no caminho exato:
+```
+android/app/src/main/kotlin/com/smgengenharia/minerasys/MainActivity.kt
